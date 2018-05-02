3e6684cc5897c8d959b60c05931126e675af138c
j991132 <j991132@naver.com>
2018-05-02 18:09:00 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pjw/quzzer/MainActivity.java
@@ -0,0 +1,315 @@
+package com.example.pjw.quzzer;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class MainActivity extends AppCompatActivity {
+
+    //일반 스레드로 실행되는 작업의 결과를 화면에 출력시키기 위해 메인 핸들러 사용
+    private Handler mMainHandler;
+
+    //데이터를 서버에 전송  또는 일반 스레드를 종료시키는 기능을 수행하는 루퍼 변수와 핸들러스레드 변수선언
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+    private HandlerThread thread;
+
+    //서버로부터 데이터 수신받는 기능을 수행하는 일반스레드 변수선언
+    private TCPClient client = null;
+    private Socket socket;
+    private BufferedReader networkReader = null;
+    private BufferedWriter networkWriter = null;
+
+    //화면의 버튼과 텍스트 변수 선언
+    private Button connect;
+    private Button finish;
+    private Button start;
+    private TextView text;
+
+    private String ip;
+    private int port = 6000;
+    private String TAG = "TcpClientActivity";
+
+    //핸들러가 수행하는 기능은 아래 상수에 의해 결정
+    public static final int MSG_CONNECT=1;
+    public static final int MSG_STOP=2;
+    public static final int MSG_CLIENT_STOP=3;
+    public static final int MSG_SERVER_STOP=4;
+    public static final int MSG_START=5;
+    public static final int MSG_ERROR=999;
+
+    @Override
+    public void onDestroy(){
+        //화면을 닫으면 소케과 스레드 모두 종료
+        super.onDestroy();
+        //소켓 객체를 닫는 close() 메소드는 일반 스레드로 실행해야함-메인스레드에서 실행시 오류-4웨이 핸드쉐이크 때문
+        //따라서 핸들러 호출
+        if (client != null){
+            mServiceHandler.sendEmptyMessage(MSG_STOP);
+        }
+        //핸들러스레드 종료
+        thread.quit();
+        SystemClock.sleep(100); //작업이 완료될 때까지 0.1초 작업 대기
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText eip= (EditText) findViewById(R.id.editText1);
+        final EditText eport= (EditText) findViewById(R.id.editText2);
+        final EditText et= (EditText) findViewById(R.id.editText3);
+
+        connect = (Button)findViewById(R.id.button1);
+        finish =  (Button)findViewById(R.id.button2);
+        start = (Button)findViewById(R.id.button3);
+        text = (TextView)findViewById(R.id.textView1);
+
+        connect.setEnabled(true);
+        finish.setEnabled(false);
+        start.setEnabled(false);
+
+        //핸들러스레드 생성하고 실행
+        thread = new HandlerThread("HandlerThread");
+        thread.start();
+
+        //핸들러스레드로 부터 루퍼 얻기
+        mServiceLooper = thread.getLooper();
+
+        //핸들러스레드가 제공한 루퍼객체를 매개변수로 해서 핸들러 객체를 생성
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+
+        //메인 스레드에서 사용하는 핸들러
+        mMainHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String m;
+                switch (msg.what) {
+                    case MSG_CONNECT:
+                        m = "정상적으로 서버에 접속하였습니다.";
+
+                        //종료와 전달버튼 활서화 & 연결버튼 비활성화-연결버튼 누를때 마다 스레드 계속 생성되기 방지
+                        connect.setEnabled(false);
+                        finish.setEnabled(true);
+                        start.setEnabled(true);
+                        text.setText(m);
+                        break;
+
+                    case MSG_CLIENT_STOP:
+                        //사용자가 연결 작업 종료시
+                        text.setText((String) msg.obj);
+                        connect.setEnabled(true);
+                        finish.setEnabled(false);
+                        start.setEnabled(false);
+                        m="클라이언트가 접속을 종료하였습니다.";
+                        break;
+
+                    case MSG_SERVER_STOP:
+                        //서버에 의해 연결이 종료될 때
+                        text.setText((String) msg.obj);
+                        connect.setEnabled(true);
+                        finish.setEnabled(false);
+                        start.setEnabled(false);
+                        m="서버가 접속을 종료하였습니다.";
+                        break;
+
+                    case MSG_START:
+                        //메세지를 전송하였을 때 호출
+                        m = "메세지 전송 완료!";
+                        text.setText((String) msg.obj);
+                        break;
+
+                    default:
+                        //에러 발생시 호출
+                        m="에러 발생!";
+                        text.setText((String) msg.obj);
+                        break;
+                }
+                Toast.makeText(MainActivity.this, m, Toast.LENGTH_SHORT).show();
+                super.handleMessage(msg);
+            }
+        };
+
+        //연결버튼 누를 때 실행
+        connect.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View V){
+                ip = eip.getText().toString();
+                try{
+                    //포트번호는 양수여야 함
+                    port = Integer.parseInt(eport.getText().toString());
+                }catch (NumberFormatException e){
+                    port = 6000;
+                    Log.d(TAG, "포트번호", e);
+                }
+                if (client == null) {
+                    try{
+                        //스레드 생성하고 서버IP 주소와 포트번호 넘겨주기
+                        client = new TCPClient(ip, port);
+                        client.start();
+                    }catch (RuntimeException e){
+                        text.setText("IP주소나 포트번호가 잘못되었습니다..");
+                        Log.d(TAG, "에러발생", e);
+                    }
+                }
+            }
+        });
+
+        finish.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View V){
+                if (client !=null){
+                    //핸들러스레드를 사용하여 스레드 종료
+                    mServiceHandler.sendEmptyMessage(MSG_CLIENT_STOP);
+                }
+            }
+        });
+
+        //데이터를 서버에 전송
+
+        start.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                if (et.getText().toString() != null){
+                    //핸들러로부터 메시지 하나 반환받는다(new로 생성하는 것이 아닌 핸들러에 있는 msg 가져오기)
+                    Message msg = mServiceHandler.obtainMessage();
+                    msg.what = MSG_START;
+                    msg.obj = et.getText().toString();
+
+                    //핸들러스레드를 통해 문자를 서버에 전달
+                    mServiceHandler.sendMessage(msg);
+                }
+            }
+        });
+
+    }
+
+    //핸들러스레드에서 사용하는 핸들러
+    private final class ServiceHandler extends Handler{
+        public ServiceHandler(Looper looper){
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case MSG_START:
+                    Message toMain = mMainHandler.obtainMessage();
+                    try{
+                        networkWriter.write((String) msg.obj);
+                        networkWriter.newLine();
+                        networkWriter.flush();
+
+                        //메시지가 성공적으로 전송되었다면 전송한 문자열을 화면에 출력
+                        toMain.what = MSG_START;
+                    }catch (IOException e){
+                        toMain.what = MSG_ERROR;
+                        Log.d(TAG, "에러발생",e);
+                    }
+                    toMain.obj = msg.obj;
+                    mMainHandler.sendMessage(toMain);
+                    break;
+                case MSG_STOP:
+                case MSG_CLIENT_STOP:
+                case MSG_SERVER_STOP:
+                    client.quit();
+                    client = null;
+                    break;
+            }
+        }
+    }
+
+    //일반스레드
+    public class TCPClient extends Thread{
+        Boolean loop;
+        SocketAddress socketAddress;
+        String line;
+        private final int connection_timeout = 6000;  //타임아웃 시간 6초
+        public TCPClient(String ip, int port){
+            //아이피 주소와 포트번호를 이용하여 SocketAddress 객체를 만든다
+            socketAddress = new InetSocketAddress(ip, port);
+        }
+
+        @Override
+        public void run(){
+            try{
+                //아래 소켓 객체를 스레드 생성자에서 만들어도 되지만
+                //아이피 주소와 포트번호를 사용하여 서버에 연결요청을 한다면 run() 안에서 구현해야 한다.
+                socket = new Socket();
+                socket.setSoTimeout(connection_timeout);
+                socket.setSoLinger(true, connection_timeout);
+
+                //블록모드로 작동하는 connect() 메소드에서 반환되면 서버와 정상적으로 연결된 것으로 인식할 수 있다.
+                socket.connect(socketAddress, connection_timeout);
+                networkWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                InputStreamReader i = new InputStreamReader(socket.getInputStream());
+                networkReader = new BufferedReader(i);
+
+                //위의 모든 작업이 정상적으로 수행되면 화면으로 연결되었음을 알린다.
+                Message toMain = mMainHandler.obtainMessage();
+                toMain.what = MSG_CONNECT;
+                mMainHandler.sendMessage(toMain);
+                loop = true;
+            }catch (Exception e){
+                loop = false;
+                Log.d(TAG, "에러발생", e);
+                Message toMain = mMainHandler.obtainMessage();
+                toMain.what = MSG_ERROR;
+                toMain.obj = "소켓을 생성하지 못했습니다.";
+                mMainHandler.sendMessage(toMain);
+            }
+
+            while (loop){
+                try{
+                    line = networkReader.readLine();
+
+                    //서버로부터 FIN 패킷을 수신하면 read() 메소드는 null을 반환
+                    if (line == null)
+                        break;
+
+                    //읽어들인 문자열은 화면 출력을 위해 Runnable 객체와 핸들러를 사용하였다.
+                    //메시지를 사용하지 않은 이유는 단지 이렇게 하더라도 작동하는지 보여주기 위함
+                    Runnable showUpdate = new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText(line);
+                        }
+                    };
+                    mMainHandler.post(showUpdate);
+                }catch (InterruptedIOException e){
+                }catch (IOException e){
+
+                    //소켓을 닫거나 또는 네트워크에 문제가 발생하면 대기하고 있던 read() 메소드에서 예외 발생
+                    Log.d(TAG, "에러발생", e);
+                    break;
+                }
+            }
+            //루프에서 빠져나오면 스레드 종료를 위해 소켓 객체와 스트림 객체를 닫는다.
+            try{
+                
+            }
+        }
+
+    }
+}
---
+++ lib/src/main/java/com/example/pjw/lib/buzzerSever.java
@@ -0,0 +1,209 @@
+package com.example.pjw.lib;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Handler;
+
+public class buzzerSever {
+    public  static void main(String[] args)
+    {
+        try{
+            //서버소켓() 객체 생성시키는 스레드를 생성하고 실행
+            ServerThread echothread = new ServerThread(6000, 10);
+            echothread.start();
+
+            //서버를 종료하고 자 한다면 'exit'의 입력을 받는다.
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter \"exit\" to quit.");
+
+            //콘솔로부터 데이터가 입력될 때까지 대기
+            String host;
+            do{
+                host = in.readLine();
+            }while (! host.equalsIgnoreCase("exit"));
+
+            //서버소켓을 닫고 스레드를 종료
+            echothread.shutdown();
+            echothread.join();
+            System.out.println("Completed shutdown.");
+        }catch (Exception e){
+            //에러 발생시 작업종료
+            System.err.println("Interruped before accept thread completed."+ e.toString());
+            System.exit(1);
+        }
+    }//main
+}
+
+//ServerThread 클래스는 쓰레드 클래스를 상속받아 별도의 클래스로 생성
+
+class ServerThread extends Thread{
+    private Boolean loop;
+    private final ServerSocket server;
+    private final ExecutorService pool;
+
+    //List 는 핸들러로 실시간 클라이언트와 연결상태 관리
+    //리스트 요소는 개별 스레드로부터 언제든 삭제 추가가 가능하므로 Lock 기능이 필요
+    private final List<Handler> threadList;
+    private final ReentrantLock lock;
+
+    public ServerThread(int port, int poolSize) throws IOException{
+        super();
+        server = new ServerSocket(port);
+        server.setSoTimeout(3000); // 공간이 교실 하나정도 크기면 3초면 적당
+
+        //실행되는 스레드 수를 제한하기 위해 스레드 풀 사용
+        pool = Executors.newFixedThreadPool(poolSize);
+
+        //ExcutorService 객체를 삭제한다고 해서 현재 실행중인 개별 태스크는 삭제되지 않음
+        //따라서 개별 태스크를 삭제 시키기 위해 리스트 객체를 생성
+        threadList = new ArrayList<Handler>();
+        lock = new ReentrantLock();
+        loop = true;
+    }// 생성자
+
+    @Override
+    public void run(){
+        System.out.println("접속을 기다립니다.");
+        while (loop){
+            try{
+                //타임아웃이 발생하면 InterruptedIOException 예외가 발생
+                Handler h = new Handler(server.accept());
+                lock.lock(); //요소 추가를 위해 잠금을 설정
+                threadList.add(h);
+                lock.unlock();
+
+                pool.execute(h); //스레드풀 내부에서 태스크를 실행
+            }catch (InterruptedIOException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                //타임아웃 이외의 예외 발생시 스페드풀 닫고 모든 작업 종료
+                pool.shutdown();
+                System.out.println("스레드에서 예외가 발생하였습니다."+e.toString());
+                break;
+            }
+        }
+
+        //서버소켓을 닫는다.
+        try{
+            server.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void shutdown() {
+        pool.shutdown(); //스레드 풀을 닫는다.
+        try {
+            //작업이 완료될 때까지 0.5초 대기
+            if (!pool.awaitTermination(500L, TimeUnit.MILLISECONDS)) {
+                pool.shutdownNow(); //스레드 풀을 즉시 닫기
+                if (!pool.awaitTermination(500L, TimeUnit.MILLISECONDS)) {
+                    System.err.println("Pool did not terminate");
+                    //스레드 풀이 해제되지 않는다는 것은 아직 클라이언트와 접속되어 있다는 것
+                    //따라서 강제로 접속을 해제
+                    clearlist();
+                }
+            }
+        }catch (InterruptedException ie){
+            pool.shutdownNow();
+            clearlist();
+        }
+        //소켓서버의 모든 작업을 종료
+        loop = false;
+    }
+    private void clearlist() {
+        //리스트의 요소들을 찾아 강제 해제
+        if (!threadList.isEmpty()){
+            lock.lock();
+            for (int index = 0; index < threadList.size(); ++index){
+                //태스크 내 quit() 메소드를 호출하여 작업 종료
+                threadList.get(index).quit();
+            }
+            lock.unlock();
+        }
+    }
+    // Handler 내부 클래스
+class  Handler implements Runnable{
+    private Socket sock;
+
+    //외부에서 생성된 객체는 생성자의 매개변수로 전달받아 내부 로컬 변수에 입력하여 사용
+        public Handler(Socket sock){
+            this.sock = sock;
+        }
+
+        public void quit() {
+            if(sock != null){
+                try{
+                    //소켓을 닫는다면 readLine() 메소드에서 IOException 예외가 발생함
+                    sock.close();
+                    sock = null;
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                }
+            }
+
+            @Override
+        public void run(){
+            try{
+                InetAddress inetaddr = sock.getInetAddress();
+                System.out.println(inetaddr.getHostAddress() + "이 접속하였습니다.");
+
+                OutputStream out = sock.getOutputStream();
+                InputStream in = sock.getInputStream();
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+
+                //윈도우에서 인코딩은 MS949를 사용한다
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(in, "MS949"));
+                String line = null;
+                try{
+                    //클라이언트로부터 메시지가 올 때까지 대기한다.
+                    while ((line = br.readLine()) != null){
+                        System.out.println(" 클라이언트로부터 전송받은 문자열: "+line);
+
+                        //클라이언트로부터 'quit'메세지가 온다면 작업을 종료시킨다.
+                        //소켓을 닫는다면 readLine() 메소드에서 IOException 예외가 발생한다.
+                        if (line.equalsIgnoreCase("quit")) quit();
+
+                        pw.println(line);
+                        pw.flush();
+                    }
+                }catch (IOException e){
+                    // quit() 메소드를 호출하여 소켓을 닫거나 또는 네트워크 내 문제가 발생시 예외처리
+                }finally {
+                    //객체 종료와 함께 리스트 객체 내 보관중인 요소에서 객체를 찾아 삭제
+                    lock.lock();
+                    threadList.remove(this);
+                    lock.unlock();
+
+                    System.out.println(inetaddr.getHostAddress()+"이 접속을 종료하였습니다.");
+                    if(pw !=null) pw.close();
+                    if(br !=null) br.close();
+                    if(sock !=null) sock.close();
+                }
+            }catch (Exception ex){
+                System.out.println(ex);
+            }
+
+        }// run
+    } // Handler 내부 클래스
+
+
+
+}// ServerThread 클래스
